034adedece25aaeaa9159051479b6509e682e697
uavendano <uavendano@taec.com.mx>
2025-10-13 15:37:04 -0600

    dev

--- backend/src/controllers/calendarioTiendaController.js
+++ backend/src/controllers/calendarioTiendaController.js
@@ -2,20 +2,20 @@ import { sequelize2 } from "../config/databaseTienda.js";
 
 // Obtener fechas por ID
 export const getDatesById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const [resultados] = await sequelize2.query(
-      "SELECT * FROM v_cursos WHERE id = ?",
-      { replacements: [id] }
-    );
+    try {
+        const { ids } = req.query; // Ej: ?ids=7,8,9
+        if (!ids) return res.status(400).json({ error: "No se proporcionaron IDs" });
 
-    if (resultados.length === 0) {
-      return res.status(404).json({ message: "No hay fechas disponibles" });
+        const idArray = ids.split(','); // Convertimos a array ["7","8","9"]
+
+        const [resultados] = await sequelize2.query(
+        `SELECT * FROM v_cursos WHERE id IN (${idArray.map(() => '?').join(',')})`,
+        { replacements: idArray }
+        );
+
+        res.json(resultados);
+    } catch (error) {
+        console.error("Error al obtener fechas por IDs:", error);
+        res.status(500).json({ error: "Error interno del servidor" });
     }
-
-    res.json(resultados);
-  } catch (error) {
-    console.error("Error al obtener fechas por ID:", error);
-    res.status(500).json({ error: "Error interno del servidor" });
-  }
 };
--- backend/src/controllers/customersController.js
+++ backend/src/controllers/customersController.js
@@ -1,4 +1,5 @@
 import customersModel from '../models/customersModel.js';
+import { logInfo, logError, logWarn } from '../utils/logger.js';
 
 export const getAllCustomer  = async (req, res) => {
     try {
@@ -8,7 +9,7 @@ export const getAllCustomer  = async (req, res) => {
         res.status(200).json(customer);
     } catch (error) {
         console.error('Error al obtener todos los registros de los clientes:', error);
-        logger.error('Error al obtener todos los registros de los clientes:', error);
+        logError('Error al obtener todos los registros de los clientes:', error);
         res.status(500).json({ message: 'Error al obtener todos los registros de los clientes:', error: error.message });
     }
 };
